0386144572b62c32b579ee026490baf58d287761
Ezdim <poyrazsarsilmaz1903@gmail.com>
2026-06-24 23:23:59 +0300

    Update set_webhook.php

--- set_webhook.php
+++ set_webhook.php
@@ -1,6 +1,6 @@
 <?php
 $token = json_decode(file_get_contents('token.json'), true)['token'];
-$webhookUrl = 'https://camera-41g4.onrender.com/abo.php';
+$webhookUrl = 'https://camera-4lg4.onrender.com/abo.php';
 $api = "https://api.telegram.org/bot$token/setWebhook?url=" . urlencode($webhookUrl);
 echo file_get_contents($api);
 ?>
